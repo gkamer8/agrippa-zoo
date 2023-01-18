4371f911a183c30a81503806f85d9b114669539b
Gordon Kamer <gkamer@college.harvard.edu>
2023-01-17 22:31:08 -0500

    register auth

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -6,7 +6,7 @@ from flask_cors import cross_origin
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route('/login', methods=['POST'])
 @cross_origin()
 def login():
 
@@ -27,3 +27,30 @@ def login():
 
     return json.dumps({'response': 'succeeded'})
 
+@bp.route('/register', methods=['POST'])
+@cross_origin()
+def register():
+
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
+
+    if not username:
+        error = "{'response': 'failed', 'why': 'username_missing'}"
+    elif not password:
+        error = "{'response': 'failed', 'why': 'password_missing'}"
+
+    if error is None:
+        try:
+            db.execute(
+                "INSERT INTO users (username, password_hash) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = "{'response': 'failed', 'why': 'username_taken'}"
+        else:
+            return "{'response': 'succeeded'}"
+
+    return error
